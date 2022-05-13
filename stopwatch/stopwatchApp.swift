cb708fde804b24ce7ff4a1a678101685c88a0c94
Swapkumbhar31 <swapkumbhar31@gmail.com>
2022-05-13 17:01:31 -0400

    Initial Commit

---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by Swapnil Kumbhar on 2022-05-13.
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
